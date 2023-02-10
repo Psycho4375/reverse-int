f49cef77b2513e3ad7d1208b3298e3f38e9f10b6
Psycho4375 <119705159+Psycho4375@users.noreply.github.com>
2023-02-10 16:07:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let result = 0;
+
+  while (n > 0){
+    rightMost = n % 10;
+    result = result * 10 + rightMost;
+    n = Math.floor(n/10);
+  }
+  return result;
 }
